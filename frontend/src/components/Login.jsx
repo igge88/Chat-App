b6cd6e228dd7c088624ef3a79fa4d97ca0a4e202
igge <igge88@hotmail.com>
2023-06-02 15:48:33 +0200

    push

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
+/*
 import axios from 'axios'
 import {useState, useEffect} from 'react'
 
@@ -67,19 +67,59 @@ export const Login = (props) => {
 }
 
 export default Login
-
-
-/*
-import React, { useState } from 'react'
-
-export const Login = () => {
-    return (
-        <form>
-            <label htmlFor="username">Username</label>
-            <input type="text" placeholder="yourusername" id="username" name="username" />
-            <label htmlFor="password">Password</label>
-            <input type="password" placeholder="*******" id="password"/>
-        </form>
-    )
-}
 */
+/* eslint-disable react/no-unescaped-entities */
+import axios from 'axios';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export const Login = (props) => {
+  const history = useHistory();
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Make the login request
+      const response = await axios.post('http://localhost:8800/login', formData);
+      // Assuming the server returns a user object in the response
+      const user = response.data.user;
+      // Redirect to the chat page
+      history.push('/chat');
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  };
+
+  return (
+    <>
+      <div className="form-container">
+        <h2>Log In</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" onChange={handleChange} />
+          <label htmlFor="password">Password</label>
+          <input type="password" placeholder="********" id="password" name="password" onChange={handleChange} />
+          <button type="submit">Log In</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('register')}>
+          Don't have an account? Register here.
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Login;
